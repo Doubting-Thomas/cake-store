d743039bc1d51dbbac3b1a71531ac00858316f55
Doubting-Thomas <thomasmichaelayo@gmail.com>
2022-12-27 05:36:03 -0800

    feat: basic add to cart test for now

---
+++ cake-store/js/model.js
@@ -0,0 +1,15 @@
+const btnAdd = document.querySelector(".btn-info");
+const cartCount = document.querySelector(".cart-count");
+
+btnAdd.addEventListener("click", function (e) {
+  if (e.target.classList.contains("btn")) {
+    cartCount.classList.remove("hidden");
+    console.log("clicked");
+  }
+});
+
+// btnAdd.addEventListener("click", function (e) {
+//   if (e.target.matches("button.btn")) {
+//     console.log("clicked");
+//   }
+// });
