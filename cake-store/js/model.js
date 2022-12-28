b98eb930bd33337bea5c7a678aabb31344ff04b8
Doubting-Thomas <thomasmichaelayo@gmail.com>
2022-12-28 03:25:12 -0800

    fix: change classlist to  toggle

--- cake-store/js/model.js
+++ cake-store/js/model.js
@@ -2,7 +2,7 @@ const navBtn = document.querySelector(".nav__cart--btn");
 const navCart = document.querySelector(".cart");
 
 function revealCart() {
-  navCart.classList.remove("hidden");
+  navCart.classList.toggle("hidden");
 }
 
 navBtn.addEventListener("click", revealCart);
