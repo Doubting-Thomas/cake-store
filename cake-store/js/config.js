684400798ac0c7ee36b7d94e57a3d4292fd101a0
Doubting-Thomas <thomasmichaelayo@gmail.com>
2022-12-28 05:05:14 -0800

    feat: add global variables file and add to cart

---
+++ cake-store/js/config.js
@@ -0,0 +1,3 @@
+export const navBtn = document.querySelector(".nav__cart--btn");
+export const navCart = document.querySelector(".cart");
+export const btnCart = document.querySelectorAll(".btn-info");
--- cake-store/js/model.js
+++ cake-store/js/model.js
@@ -1,8 +1,40 @@
-const navBtn = document.querySelector(".nav__cart--btn");
-const navCart = document.querySelector(".cart");
-
+import { btnCart, navBtn, navCart } from "./config.js";
+// Display the cart
 function revealCart() {
   navCart.classList.toggle("hidden");
 }
+console.log("yes");
 
+//Add items to the cart
+function addToCart() {
+  btnCart.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+      if (e.target.parentElement.classList.contains("btn-info")) {
+        console.log(
+          e.target.parentElement.previousElementSibling.previousElementSibling
+            .firstChild.nextSibling.src
+        );
+      } else {
+        console.log("err");
+      }
+    });
+  });
+}
+
+// function addToCart() {
+//   btnCart.forEach(function (btn) {
+//     btn.addEventListener("click", function (e) {
+//       // if (e.target.classList.contains("product__btn")) {
+//       //   console.log(e.target);
+//       // } else {
+//       //   console.log("err");
+//       // }
+//       const clicked = e.target.closest("product__img");
+//       console.log(clicked);
+//     });
+//   });
+// }
+addToCart();
 navBtn.addEventListener("click", revealCart);
+
+// btnCart.addEventListener("click", addToCart);
