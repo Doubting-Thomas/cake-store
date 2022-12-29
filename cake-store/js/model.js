c87f96c071540074c9c3b799acaf9417ad62c10b
Doubting-Thomas <thomasmichaelayo@gmail.com>
2022-12-29 02:23:42 -0800

    feat: add functionality for adding items to cart

--- cake-store/js/model.js
+++ cake-store/js/model.js
@@ -10,16 +10,84 @@ function addToCart() {
   btnCart.forEach(function (btn) {
     btn.addEventListener("click", function (e) {
       if (e.target.parentElement.classList.contains("btn-info")) {
-        console.log(
+        // let come =
+        //   e.target.parentElement.previousElementSibling.previousElementSibling
+        //     .firstChild.nextSibling;
+        // come.classList.add("cart-mini");
+        // console.log(come);
+
+        // console.log(
+        //   e.target.parentElement.previousElementSibling.previousElementSibling
+        //     .firstChild.nextSibling.src
+        // );
+
+        let imgSrc =
           e.target.parentElement.previousElementSibling.previousElementSibling
-            .firstChild.nextSibling.src
-        );
+            .firstChild.nextSibling;
+        imgSrc.classList.add("cart-mini");
+        console.log(imgSrc);
+
+        imgSrc =
+          e.target.parentElement.previousElementSibling.previousElementSibling
+            .firstChild.nextSibling.src;
+        let imgPos = imgSrc.indexOf("img");
+        let imgPath = imgSrc.slice(imgPos);
+        console.log(imgPath);
+
+        const cartItem = {};
+        cartItem.img = imgPath;
+        let cartItemName =
+          e.target.parentElement.previousElementSibling.firstChild
+            .nextElementSibling.textContent;
+        cartItem.name = cartItemName;
+        let cartItemPrice =
+          e.target.parentElement.previousElementSibling.firstChild
+            .nextElementSibling.nextElementSibling.textContent;
+        cartItem.price = cartItemPrice;
+        console.log(cartItem);
+
+        // Insert the values into the HTML
+        const addCartItem = document.createElement("div");
+        addCartItem.classList.add("cart__item");
+
+        addCartItem.innerHTML = `
+        <img
+          src="${cartItem.img}"
+          class="cart-mini"
+          alt="cake-img"
+        />
+        <div class="cart__text">
+          <p class="cart__title">${cartItem.name}</p>
+          <span class="cart__price">${cartItem.price}</span>
+        </div>
+        <a href="#" class="cart-item-remove">
+          <img src="img/icons8-trash-can-24.png" alt="trash-icon" />
+        </a>
+      </div>
+    `;
+        console.log(addCartItem);
+
+        navCart.insertAdjacentElement("beforeend", addCartItem);
       } else {
         console.log("err");
       }
     });
   });
 }
+
+// function addToCart() {
+//   btnCart.forEach(function (btn) {
+//     btn.addEventListener("click", function (e) {
+//       // if (e.target.classList.contains("product__btn")) {
+//       //   console.log(e.target);
+//       // } else {
+//       //   console.log("err");
+//       // }
+//       const clicked = e.target.closest("product__img");
+//       console.log(clicked);
+//     });
+//   });
+// }
 
 addToCart();
 navBtn.addEventListener("click", revealCart);
