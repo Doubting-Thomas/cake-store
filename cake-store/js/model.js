6993397911f6fc72fac2e50e6f98a3c273ae791a
Doubting-Thomas <thomasmichaelayo@gmail.com>
2022-12-31 00:21:07 -0800

    feat: display car total text/values

--- cake-store/js/model.js
+++ cake-store/js/model.js
@@ -5,6 +5,7 @@ import {
   cartScore,
   cartCount,
   cartTotalValue,
+  cartTotalText,
 } from "./config.js";
 
 // Add items to the cart
@@ -59,6 +60,7 @@ function addToCart() {
         navCart.insertAdjacentElement("afterbegin", addCartItem);
         alert("Successfully added to cart!");
         cartCount.classList.remove("hidden");
+        cartTotalText.classList.remove("hidden");
         totalPrice();
       }
     });
